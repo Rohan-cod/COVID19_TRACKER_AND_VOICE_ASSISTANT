1c75c19b480056be073e1155455b0d16f5fa101f
rohangupta <rohaninjmu@gmail.com>
2020-05-19 17:23:22 +0530

    heroku deployment

--- templates/voice.php
+++ templates/voice.php
@@ -6,3 +6,22 @@
   echo $output;
 ?>
 </head>
+
+<script>
+      $(document).ready(function () {
+
+        $('.first-button').on('click', function () {
+
+          $('.animated-icon1').toggleClass('open');
+        });
+        $('.second-button').on('click', function () {
+
+          $('.animated-icon2').toggleClass('open');
+        });
+        $('.third-button').on('click', function () {
+
+          $('.animated-icon3').toggleClass('open');
+        });
+      });
+      
+    </script>
